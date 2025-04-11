64983542cf17efd8aa75a32c951b047a4d519c2e
ChanatpakornD <csirintronsopon8@gmail.com>
2025-04-11 15:28:34 +0700

    init: move backend

--- frontend/src/auth.ts
+++
@@ -1,6 +0,0 @@
-//TODO: AUTH HERE
-
-// import NextAuth from 'next-auth'
-// import 'next-auth/jwt'
-// import Google from 'next-auth/providers/google'
-// import { NextRequest } from 'next/server'
--- frontend/src/types/message.ts
+++
@@ -1,8 +0,0 @@
-export interface Message {
-  id: string
-  chatId: string
-  senderId: string
-  text: string
-  timestamp: string
-  isEdited: boolean
-}
