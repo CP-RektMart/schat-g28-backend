7f311c68d433449d7715f3dbfe5807fd834e1ac4
TeeGoood <teegooodgood@gmail.com>
2025-04-23 02:09:00 +0700

    sdf

--- internal/services/group/get-group-by-id.go
+++ internal/services/group/get-group-by-id.go
@@ -30,7 +30,7 @@ func (h *Handler) HandleGetByID(c *fiber.Ctx) error {
 		return apperror.BadRequest("invalid request", err)
 	}
 
-	group, err := h.repo.GetByID(req.ID, "Members", "Messages")
+	group, err := h.repo.GetByID(req.ID, "Members", "Messages", "Owner")
 	if err != nil {
 		return errors.Wrap(err, "failed fetch user")
 	}
